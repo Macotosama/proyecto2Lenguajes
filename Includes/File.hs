5d0c97e759ec00a7427e4bb67cf3cc1e5b891680
Macotosama <jtn1999@outlook.com>
2022-09-27 02:04:23 -0600

    Se termino la carga y mostra de parqueos

--- Includes/File.hs
+++ Includes/File.hs
@@ -1,12 +1,18 @@
 module Includes.File (
     split,
     convertirString,
-    fileToMatrix,
+    arreglarString,
+    obtenerLineas,
     leerArchivo,
-    obtenerArchivo
-)
-where
+    obtenerArchivo,
+    filasString,
+    listaString,
+    escribirArchivo
+) where
 import System.IO (withFile, IOMode (ReadMode), hGetContents)
+import System.IO.Unsafe (unsafePerformIO)
+import Data.List (sort,group)
+import Data.IORef
 
 -- Separa un string si es igual a un token
 split :: (Char -> Bool) -> [Char] -> [[Char]]
@@ -24,8 +30,8 @@ convertirString list newList = do
     else convertirString (init list) newList ++ [split (== ',') (last list)]
 
 -- Convierte una lista de strings y las convierte en una matriz
-fileToMatrix :: [[Char]] -> [[[Char]]]
-fileToMatrix list = do
+arreglarString :: [[Char]] -> [[[Char]]]
+arreglarString list = do
   if null list
     then [[]]
     else convertirString list []
@@ -33,10 +39,36 @@ fileToMatrix list = do
 --Lee un archivo por medio de su ruta
 leerArchivo :: FilePath -> IO [Char]
 leerArchivo ruta = withFile ruta ReadMode $ \handle -> do
-    theContent <- hGetContents handle
-    mapM return theContent
+  theContent <- hGetContents handle
+  mapM return theContent
+
+obtenerLineas :: FilePath -> IO [String] --Lista de las lineas del archivo
+obtenerLineas path = do
+  str <- leerArchivo path
+  return (lines str)
 
 --Busca el archivo y regresa los datos del archivo en un archivo
+obtenerArchivo :: FilePath -> IO [[[Char]]]
 obtenerArchivo ruta = do
-    archivo <- leerArchivo ruta
-    return (fileToMatrix archivo)
+    archivo <- obtenerLineas ruta
+    return (arreglarString archivo)
+
+--Convierte una lista a un string
+filasString :: [[Char]] -> [Char]
+filasString list
+  | null list = return '\n'
+  | length list == 1 = head list++filasString (tail list)
+  | otherwise = head list++","++filasString (tail list)
+
+--Convierte una matriz a string
+listaString :: [[[Char]]] -> [Char]
+listaString matrix = do
+    if null matrix then
+      ""
+    else filasString (head matrix) ++ listaString (tail matrix)
+
+--Elimina todo el contenido de un archivo y agrega nueva información
+escribirArchivo :: FilePath -> [[[Char]]] -> IO ()
+escribirArchivo path newData = do
+  let newDataStr = listaString newData
+  writeFile path newDataStr
--- main.hs
+++ main.hs
@@ -1,58 +1,101 @@
 import Includes.Auth (confirmarUsuario)
-import Includes.File (obtenerArchivo)
+import Includes.File (split,
+  convertirString,
+  arreglarString,
+  obtenerLineas,
+  leerArchivo,
+  obtenerArchivo,
+  filasString,
+  listaString,
+  escribirArchivo)
 import Control.Monad (unless, when)
+import Text.XHtml (dir)
+import System.Win32 (COORD(y))
+
+nuevoParqueo :: String -> IO ()
+nuevoParqueo parqueos = do
+  parqueos <- obtenerArchivo parqueos
+  putStrLn "Ingrese el nombre del parqueo"
+  nombre <- getLine
+  putStrLn "Ingrese la direccion del parqueo"
+  dir <- getLine
+  putStrLn "Ingrse las provincias del parqueo"
+  pro <- getLine
+  putStrLn "Ingrese las coordenadas x del parqueo"
+  equis <- getLine
+  putStrLn "Ingrese las coordenadas y del parqueo"
+  ye <- getLine
+  escribirArchivo "./datos/parqueos.csv" (take 1 parqueos ++ [[nombre, dir, pro, equis, ye]])
+
+menuParqueos :: String -> IO ()
+menuParqueos parqueos = do
+  putStrLn "Parqueos"
+  putStrLn "1- Cargar parqueo"
+  putStrLn "2- Mostrar parqueos"
+  putStrLn "3- Volver"
+  option <- getLine
+  case option of
+    "1" -> nuevoParqueo parqueos
+    "2" -> putStrLn "En espera"
+    "3" -> return ()
+    _ -> putStrLn "Escoja una opcion valida"
+  Control.Monad.when (option /= "3") (menuParqueos parqueos)
 
 -- Muestra el menu operativo
-menuOperativo :: IO ()
-menuOperativo = do
-    putStrLn "Bienvenido al sistema de administración de bicicletas"
+menuOperativo :: String -> String -> String -> IO ()
+menuOperativo parqueos bicicletas usuarios = do
+    putStrLn "Menu operativo"
     putStrLn "Escoja una de las siguientes opciones"
     putStrLn "1- Cargar y Mostrar parqueos"
     putStrLn "2- Mostrar bicicletas"
     putStrLn "3- Cargar usuarios"
     putStrLn "4- Estadísticas"
     putStrLn "5- Volver"
-    putStr ">>"
     option <- getLine
     case option of  
-        "1" -> putStrLn "Escoja una opcion valida"
+        "1" -> menuParqueos parqueos
         "2" -> putStrLn "Escoja una opcion valida"
         "3" -> putStrLn "Escoja una opcion valida"
         "4" -> putStrLn "Escoja una opcion valida"
         "5" -> return ()
         _ -> putStrLn "Escoja una opcion valida"
-    Control.Monad.when (option /= "5") menuOperativo
+    Control.Monad.when (option /= "5") (menuOperativo parqueos bicicletas usuarios)
 
 -- Verifica el usuario entrante
-auntentificacion :: IO ()
-auntentificacion = do
+auntentificacion :: String -> String -> String -> IO ()
+auntentificacion parqueos bicicletas usuarios = do
     putStrLn "Usuario: "
     user <- getLine
     putStrLn "Contrasena: "
     contrase <- getLine
     if confirmarUsuario user contrase then
-        menuOperativo
+        menuOperativo parqueos bicicletas usuarios
     else 
-        auntentificacion
+        auntentificacion parqueos bicicletas usuarios
 
 -- Muestra el menu principal
-menuPrincipal :: IO ()
-menuPrincipal = do
+menuPrincipal :: String -> String -> String -> IO ()
+menuPrincipal parqueos bicicletas usuarios = do
   putStrLn "Escoja una de las siguientes opciones"
   putStrLn "1- Menu operativo"
   putStrLn "2- Imprimir información"
   putStrLn "3- Salir"
-  putStr ">>"
   option <- getLine
   case option of  
-    "1" -> auntentificacion
-    "2" -> menuPrincipal
+    "1" -> auntentificacion parqueos bicicletas usuarios
+    "2" -> menuPrincipal parqueos bicicletas usuarios
     "3" -> return ()
     _ -> putStrLn "Escoja una opcion valida"
-  Control.Monad.when (option /= "3") menuPrincipal
+  Control.Monad.when (option /= "3") (menuPrincipal  parqueos bicicletas usuarios)
 
 -- Inicia el programa y llama el menu principal
 main :: IO ()
 main = do
   putStrLn "Bienvenido al sistema de administración de bicicletas"
-  menuPrincipal
+  putStrLn "Ingrese la ruta del archivo de parqueos"
+  parqueos <- getLine
+  putStrLn "Ingrese la ruta del archivo de bicicletas"
+  bicicletas <- getLine
+  putStrLn "Ingrese la ruta del archivo de usuario"
+  usuarios <- getLine
+  menuPrincipal parqueos bicicletas usuarios
